c55dc076915e2d838de71616464241b605baf70e
BhuvanBose <68459580+bhuvantca@users.noreply.github.com>
2024-04-23 06:25:12 -0700

    Update std_future_and_async.cpp

--- std_future_and_async.cpp
+++ std_future_and_async.cpp
@@ -1,1 +1,23 @@
+//STD FUTURE & STD ASYNC
+
+#include<iostream>
+#include<future>
+using namespace std;
+
+int add(int x, int y)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    int out = x + y;
+    return out;
+}
+
+int main()
+{
+    future<int> ft=std::async(add,5,6);
+    
+    //tasks
+    int sum = ft.get();
+    cout<<"result is: "<<sum;
+    return 0;
+}
 
